324f62e149cc65bbfebf18e3b0dded09e1435088
keelamvenu <venu@kmccorp.in>
2022-04-13 19:28:03 +0530

    latest

--- UIService/src/main/java/com/kmccorp/UIService/UiServiceApplication.java
+++ UIService/src/main/java/com/kmccorp/UIService/UiServiceApplication.java
@@ -2,8 +2,10 @@ package com.kmccorp.UIService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class UiServiceApplication {
 
 	public static void main(String[] args) {
